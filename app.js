8c1b890fa2639634ba6f9c15a54d411b24992c8d
AfsarAI <mohammadafsar786921@gmail.com>
2024-12-18 07:44:13 +0530

    hello try!

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-// app.js
 var app = new Vue({
-    el: '#app', // Selects the element with ID "app"
-    data: {
-      msg: 'Hey Man!' // Data property for the template
+  el: '#app',
+  data: {
+    msg: '', // Initially empty message
+    submittedMsg: '' // To store the submitted message
+  },
+  methods: {
+    submitMessage() {
+      if (this.msg.trim() !== '') {
+        this.submittedMsg = this.msg; // Save the message on submit
+        this.msg = ''; // Clear the input field after submission
+      } else {
+        alert('Please enter a message before submitting!');
+      }
     }
-  });
+  }
+});
